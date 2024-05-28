8cb50ba94b5c882d106ec07c7f2a4f9d86efd520
kawkaw5000 <lexeramorcillo@gmail.com>
2024-05-28 23:51:50 +0800

    added Order Items

---
+++ app/Filament/Resources/OrderResource.php
@@ -0,0 +1,285 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\OrderResource\Pages;
+use App\Filament\Resources\OrderResource\RelationManagers;
+use App\Models\Order;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\Group;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\Textarea;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Components\Select;
+use Filament\Tables\Filters\SelectFilter; 
+use Filament\Forms\Components\MarkdownEditor;
+use Illuminate\Support\Str;
+use Filament\Forms\Set;
+use Filament\Forms\Get;
+use Filament\Tables\Actions\ActionGroup;
+use Filament\Forms\Components\ToggleButtons;
+use Filament\Forms\Components\Placeholder;
+use Illuminate\Support\Number;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\Hidden;
+use Filament\Tables\Actions\ViewAction;  
+use Filament\Tables\Actions\EditAction;  
+use Filament\Tables\Actions\DeleteAction; 
+use Filament\Tables\Actions\BulkActionGroup; 
+use Filament\Tables\Actions\DeleteBulkAction;
+use App\Models\Product;
+use Faker\Provider\ar_EG\Text;
+use Filament\Tables\Columns\SelectColumn;
+
+class OrderResource extends Resource
+{
+    protected static ?string $model = Order::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-shopping-bag';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Group::make()->schema([
+                    Section::make('Order Information')->schema([
+                        Select::make('user_id')
+                            ->label('customer')
+                            ->relationship('user', 'name')
+                            ->searchable()
+                            ->preload()
+                            ->required(),
+                        
+                        Select::make('payment_method')
+                            ->options([
+                                'gcash' => 'Gcash',
+                                'cod' => 'Cash on Delivery'
+                            ])
+                            ->required(),
+
+                        Select::make('payment_status')
+                            ->options([
+                                'pending' => 'Pending',
+                                'paid' => 'Paid',
+                                'failed' => 'Failed'
+                            ])
+                            ->default('pending')
+                            ->required(),
+
+                        ToggleButtons::make('status')
+                            ->inline()
+                            ->default('new')
+                            ->required()
+                            ->options([
+                                'new' => 'New',
+                                'processing' => 'Processing',
+                                'shipped' => 'Shipped',
+                                'deliverd' => 'Delivered',
+                                'canceled' => 'Cancelled'
+                            ])
+                            ->colors([
+                                'new' => 'info',
+                                'processing' => 'warning',
+                                'shipped' => 'success',
+                                'deliverd' => 'success',
+                                'canceled' => 'danger'
+                            ])
+                            ->icons([
+                                'new' => 'heroicon-m-sparkles',
+                                'processing' => 'heroicon-m-arrow-path',
+                                'shipped' => 'heroicon-m-truck',
+                                'deliverd' => 'heroicon-m-check-badge',
+                                'canceled' => 'heroicon-m-x-circle'
+                            ]),
+                        
+                        Select::make('currency')
+                            ->options([
+                                'php' => 'Php',
+                                'usd' => 'USD',
+                                'eur' => 'EUR'
+                            ])
+                            ->default('php')
+                            ->required(),
+                        
+                        Select::make('shipping_method')
+                            ->options([
+                                'fedex' => 'FeDEx',
+                                'ninja' => 'Ninja',
+                                'xpress' => 'Xpress',
+                                'dhl' => 'DHL' 
+                            ]),
+                        
+                        Textarea::make('notes')
+                            ->columnSpanFull(),       
+                    ])->columns(2),
+
+                    Section::make('Order Items')->schema([
+                        Repeater::make('items')
+                            ->relationship()
+                            ->schema([
+
+                                Select::make('product_id')
+                                    ->relationship('product', 'name')
+                                    ->searchable()
+                                    ->preload()
+                                    ->required()
+                                    ->distinct()
+                                    ->disableOptionsWhenSelectedInSiblingRepeaterItems()
+                                    ->columnSpan(4)
+                                    ->reactive()
+                                    ->afterStateUpdated(function ($state, $set) {
+                                        $set('unit_amount', Product::find($state)?->price ?? 0);
+                                    })
+                                    ->afterStateUpdated(function ($state, $set) {
+                                        $set('total_amount', Product::find($state)?->price ?? 0);
+                                    }),
+
+                                TextInput::make('quantity')
+                                    ->numeric()
+                                    ->required()
+                                    ->default(1)
+                                    ->minValue(1)
+                                    ->columnSpan(2)
+                                    ->reactive()
+                                    ->afterStateUpdated(fn ($state, Set $set, Get $get) => $set('total_amount', $state * $get('unit_amount'))),
+
+                                TextInput::make('unit_amount')
+                                    ->numeric()
+                                    ->required()
+                                    ->disabled()
+                                    ->dehydrated()
+                                    ->columnSpan(3),
+                                
+                                TextInput::make('total_amount')
+                                    ->numeric()
+                                    ->required()
+                                    ->dehydrated()
+                                    ->columnSpan(3),
+                            ])->columns(12),
+                            
+                            Placeholder::make('grand_total_placeholder')
+                                ->label('Grand Total')
+                                ->content(function (Get $get, Set $set){
+                                    $total = 0;
+                                    if(!$repeaters = $get('items')){
+                                        return $total;
+                                    }
+
+                                    foreach($repeaters as $key => $repeater){
+                                        $total += $get("items.{$key}.total_amount");
+                                    }
+                                    $set('grand_total', $total);
+                                    return Number::currency($total, 'PHP');                                 
+                                }),
+
+                                Hidden::make('grand_total')
+                                    ->default(0)
+                    ])
+                ])->columnSpanFull()
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('user.name')
+                    ->label('Customer')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('grand_total')
+                    ->numeric()
+                    ->sortable()
+                    ->money('PHP'),
+                
+                TextColumn::make('payment_method')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('payment_status')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('currency')
+                    ->sortable()
+                    ->searchable(),
+
+                TextColumn::make('shipping_method')
+                    ->sortable()
+                    ->searchable(),
+
+                SelectColumn::make('status')
+                    ->options([
+                        'new' => 'New',
+                        'processing' => 'Processing',
+                        'shipped' => 'Shipped',
+                        'deliverd' => 'Delivered',
+                        'canceled' => 'Cancelled'
+                    ])
+                    ->searchable()
+                    ->sortable(),
+                
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                ActionGroup::make([
+                    ViewAction::make(),
+                    EditAction::make(),
+                    DeleteAction::make(),
+                ])
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getNavigationBadge(): ?string
+    {
+        return static::getModel()::count();
+    }
+
+    public static function getNavigationBadgeColor(): string|array|null
+    {
+        return static::getModel()::count() > 10 ? 'danger' : 'success'; 
+    }
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListOrders::route('/'),
+            'create' => Pages\CreateOrder::route('/create'),
+            'view' => Pages\ViewOrder::route('/{record}'),
+            'edit' => Pages\EditOrder::route('/{record}/edit'),
+        ];
+    }
+}
