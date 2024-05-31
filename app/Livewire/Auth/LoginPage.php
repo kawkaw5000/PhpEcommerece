4d6a9579ee964ae9ff8d5a47937eb2a5a3d3eb40
kawkaw5000 <lexeramorcillo@gmail.com>
2024-05-31 12:44:38 +0800

    Added Login and Register

--- app/Livewire/Auth/LoginPage.php
+++ app/Livewire/Auth/LoginPage.php
@@ -2,10 +2,36 @@
 
 namespace App\Livewire\Auth;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Login')]
 class LoginPage extends Component
 {
+    public $email;
+    public $password;
+
+    public function save()
+    {
+        // Validate input
+        $this->validate([
+            'email' => 'required|email|max:255|exists:users,email',
+            'password' => 'required|min:6|max:255',
+        ]);
+
+        // Attempt to authenticate the user
+        if (!Auth::attempt(['email' => $this->email, 'password' => $this->password])) {
+            session()->flash('error', 'Invalid credentials');
+            return;
+        }
+
+        // Authentication successful, redirect the user
+        return redirect()->intended('/');
+    }
+
     public function render()
     {
         return view('livewire.auth.login-page');
